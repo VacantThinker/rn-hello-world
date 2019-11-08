34b07ef29b6331c81ac3a73cf0a3b332921fb8b1
VacantThinker <littlehappyforsmile@outlook.com>
2019-11-09 01:54:22 +0800

    Updates

---
+++ precache-manifest.135325951a5d86bd18c67bb3fe3f437b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8ebada442e7b813b653a47a8cec9b7",
+    "url": "/rn-hello-world/index.html"
+  },
+  {
+    "revision": "c0bf81ce1433c3675236",
+    "url": "/rn-hello-world/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dfa5ee4cd19165dc1e35",
+    "url": "/rn-hello-world/static/js/2.71392f81.chunk.js"
+  },
+  {
+    "revision": "c0bf81ce1433c3675236",
+    "url": "/rn-hello-world/static/js/main.4e25cf80.chunk.js"
+  },
+  {
+    "revision": "6c7a720cf1614da7ead6",
+    "url": "/rn-hello-world/static/js/runtime-main.0eb1e78d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0cd4b7c71fdcc29f4b6f10571b2faa5f.js"
+  "/rn-hello-world/precache-manifest.135325951a5d86bd18c67bb3fe3f437b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rn-hello-world/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
