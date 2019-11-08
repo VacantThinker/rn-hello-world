68c25f016c52d7f5707319b9b8e4d27e637e6619
VacantThinker <littlehappyforsmile@outlook.com>
2019-11-09 01:44:43 +0800

    Updates

---
+++ precache-manifest.1bd525dce6753e337bf50c4124538c85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e04247cf0100dc327940a0ed803090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e42b8d42b4a706e2b1b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dfa5ee4cd19165dc1e35",
+    "url": "/static/js/2.71392f81.chunk.js"
+  },
+  {
+    "revision": "6e42b8d42b4a706e2b1b",
+    "url": "/static/js/main.6ba9c46c.chunk.js"
+  },
+  {
+    "revision": "080ca4f86c54df2e4372",
+    "url": "/static/js/runtime-main.d986e8a3.js"
+  }
+]);
